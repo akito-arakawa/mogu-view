1e7a5b2bdb29b303a8906b365ea9d048cceda981
akito <kd1373996@st.kobedenshi.ac.jp>
2026-09-25 20:09:35 +0900

    feat:healthのテストファイルの追加 #95

---
+++ backend/src/tests/Feature/HealthTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HealthTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/api/health');
+        $response->assertJson([
+            "status" => "ok"
+        ]);
+    }
+}
